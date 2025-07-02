bd50c24f97ee45af803a53399adcb887010aa618
Oliver Böge <oliver.boege@gmx.de>
2025-07-02 20:28:30 +0200

    Add effext endpoint (#3)
    * add-effext-endpoint
    * Adjust versioning

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
-from .elevenlabs_request_node import ElevenlabsRequestNode
+from .elevenlabs_text_to_effect import ElevenlabsTextToEffect
+from .elevenlabs_text_to_voice import ElevenlabsTextToVoice
 
 NODE_CLASS_MAPPINGS = {
-    "ElevenlabsRequestNode": ElevenlabsRequestNode,
+    "ElevenlabsTextToVoice": ElevenlabsTextToVoice,
+    "ElevenlabsTextToEffect": ElevenlabsTextToEffect,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Elevenlabs Request Node": "Create your voice with Elevenlabs API",
+    "Elevenlabs text to voice": "Create your voice with Elevenlabs API",
+    "Elevenlabs text to effect": "Create your effect with Elevenlabs API",
 }
--- elevenlabs_text_to_voice.py
+++ elevenlabs_text_to_voice.py
@@ -5,7 +5,7 @@ import torch
 from elevenlabs import ElevenLabs
 
 
-class ElevenlabsRequestNode:
+class ElevenlabsTextToVoice:
     @classmethod
     def INPUT_TYPES(cls):
         return {
@@ -15,16 +15,14 @@ class ElevenlabsRequestNode:
                 "voice_id": ("STRING", {"default": ""}),
                 "model_id": ("STRING", {"default": "eleven_multilingual_v2"}),
             },
-            "optional": {
-                "output_format": ("STRING", {"default": "mp3_44100_128"}),
-            }
+            "optional": {}
         }
 
     RETURN_TYPES = ("AUDIO",)
     RETURN_NAMES = ("audio",)
 
     FUNCTION = "do_request"
-    CATEGORY = "Custom"
+    CATEGORY = "Elevenlabs API integration by 奥利奥"
 
     def do_request(self, api_key, text, voice_id, model_id, output_format="mp3_44100_128"):
         client = ElevenLabs(api_key=api_key)
